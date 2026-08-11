eeb6a36f5b6324a1c67edfebe8595d9629db87c6
Derkitoo <laradjane@gmail.com>
2026-08-11 16:52:36 +0200

    Connect app to Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 // Configuration publique du projet Supabase.
 // La publishable/anon key est conçue pour être utilisée dans le navigateur avec RLS activé.
 // Ne jamais placer de service_role key dans ce fichier.
-export const SUPABASE_URL = ''
-export const SUPABASE_PUBLISHABLE_KEY = ''
+export const SUPABASE_URL = 'https://pxywcrnvhejdmmlqpipq.supabase.co'
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_gKRn_blutzF1L3VhrvxIsw_ZXOi84cZ'
